b71cd5fc2babca3b5ac632d4dd28685f9e05b874
Adam Priest <sepone229@gmail.com>
2020-03-12 09:32:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(array)
   new_array = []
   array.each_with_index {|array, index|
-  new_array << ("#{index+1} #{array}")
+  new_array << ("#{index+1} #{array}.break")
   }
   return new_array
 end
