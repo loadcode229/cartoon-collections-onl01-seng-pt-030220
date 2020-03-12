4f41f76c66b961a9b9d0471269f5c29a6976b14c
Adam Priest <sepone229@gmail.com>
2020-03-12 09:14:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-
+def roll_call_dwarves(array)
+  new_array = []
+  array.each_with_index << ("")
 end
 
 def summon_captain_planet# code an argument here
