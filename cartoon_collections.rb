a6424ab34eac303632017232a461850a0a76e940
Adam Priest <sepone229@gmail.com>
2020-03-12 09:48:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,8 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |calls|
-    "#{calls}.join("!")
+    "#{calls}.join("!")"
+  end
 end
 
 def long_planeteer_calls# code an argument here
